5d917a3ba6ed89ba401c9cc8ff3bac2199729d66
Argeustools <100544716+Argeustools@users.noreply.github.com>
2022-07-23 01:39:23 +0200

    Update tiktok_reporter.py

--- tiktok_reporter.py
+++ tiktok_reporter.py
@@ -1,132 +1,64 @@
-from selenium import webdriver
-from selenium.webdriver.support.ui import Select
-import pyfiglet
-from os import system
-from time import sleep
-import sys
+import os
+try: import httpx, asyncio, tasksio, random, threading, pystyle, cursor, time, sys
+except: z = "python -m pip install "; os.system('%shttpx' % (z)); os.system('%sasyncio' % (z)); os.system('%stasksio' % (z)); os.system('%spystyle' % (z)); os.system('%scursor' % (z))
 
-chrome_options = webdriver.ChromeOptions()
-#chrome_options.add_argument('--headless')
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--disable-dev-shm-usage')
-driver = webdriver.Chrome('/your/path/for/chromedriver',chrome_options=chrome_options) #If you use Windows you must add .exe
 
-i = 0
+class TikTok:
+    
+    def __init__(self):
+        cursor.hide(); os.system('cls') if "nt" in os.name else os.system('clear')
+        self.username = pystyle.Write.Input("\n [ ? ] Username > ", pystyle.Colors.blue_to_red, interval=0.0025)
+        
+        self.report_count = 0; self.requests = 0
+        
+        self.report_type = pystyle.Write.Input("\n Report Types:\n   317 - Underage \n   9010 - Spam Posts \n   90031 - Promotion of criminal activities\n\n [ ? ] Report Type > ", pystyle.Colors.blue_to_red, interval=0.0025) 
+        if str(self.report_type) not in ("317", "9010", "90031"): return print(pystyle.Colorate.Horizontal(pystyle.Colors.blue_to_red, f' [ ! ] Invalid Report Type.', 1))
+        else: self.report_reason = str(self.report_type); print(pystyle.Colorate.Horizontal(pystyle.Colors.blue_to_red, f'\n [ * ] Sending Reports...\n', 1))
+        
+        self.user_id  = ""; self.secuid  = ""
+        
+        self.lock = threading.Lock()
+        self.getinfo()
 
-def loop1():
-    global i
-    sleep(10)
-    try:
-        driver.find_element_by_xpath("/html/body/div[4]/div[1]/div[3]/div/div[4]/div/button").click()
-    except:
-        print("You didn't solve the captcha yet. Need to refresh to avoid endless loop.")
-        driver.refresh()
-        loop1()
-    try:
-        sleep(2)
-        driver.find_element_by_xpath("//*[@id=\"sid4\"]/div/div/div/form/div/input").send_keys(vidUrl)
-        sleep(1)
-        driver.find_element_by_xpath("//*[@id=\"sid4\"]/div/div/div/form/div/div/button").click()
-        sleep(2)
-        driver.find_element_by_xpath("//*[@id=\"c2VuZC9mb2xsb3dlcnNfdGlrdG9V\"]/div[1]/div/form/button").click()
-        driver.refresh()
-        i += 1
-        total = i * 1000
-        print("Views success delivered! Total", total,"views")
-        sleep(55)
-        loop1()
-    except:
-        print("An error occured. Now will retry again")
-        driver.refresh()
-        loop1()
+    def print(self, arg): self.lock.acquire(); print(arg); self.lock.release()
+        
+    def title(self):
+        _t_ = time.time()
+        while True: 
+            os.system(f"title TikTok Reporter ^| Reports: {self.report_count} ^| Reqs: {self.requests} ^| Elapsed: {round(time.time()- _t_, 1)}s") if "nt" in os.name else sys.stdout.write(f"\x1b]2;TikTok Reporter | Reports: {self.report_count} | Reqs: {self.requests} | Elapsed: {round(time.time()- _t_, 1)}s\x07")
+            time.sleep(0.1)
+    
+    
+    def getinfo(self):
+        with httpx.Client() as client:
+            client.headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:99.0) Gecko/20100101 Firefox/99.0", "Cookie": "msToken=zgbEqIjfSC7M7QdTTpHDkpWLtnY4JnK22HiSE1iHCRGBBYY_36Gm-gMDqyGLBjpPE2svzjVPNGWyMFYUUEBwmGkr5y2qQuKmfjfTh0i2hfOsb_B7jfDrbd9a4IhjMLPyUIRNIZLqzG6PldNNXA=="}
+            DATA = client.get("https://www.tiktok.com/api/user/detail/?device_id=7098862702289995269&uniqueId=%s" % (self.username)).json()["userInfo"]["user"]
 
-def loop2():
-    sleep(10)
-    try:
-        driver.find_element_by_xpath("/html/body/div[4]/div[1]/div[3]/div/div[2]/div/button").click()
-    except:
-        print("You didn't solve the captcha yet. Need to refresh to avoid endless loop.")
-        driver.refresh()
-        loop2()
-    try:
-        sleep(2)
-        driver.find_element_by_xpath("/html/body/div[4]/div[3]/div/div/div/form/div/input").send_keys(vidUrl)
-        sleep(1)
-        driver.find_element_by_xpath("/html/body/div[4]/div[3]/div/div/div/form/div/div/button").click()
-        sleep(10)
-        driver.find_element_by_xpath("/html/body/div[4]/div[3]/div/div/div/div/div[1]/div/form/button").click()
-        sleep(10)
-        hearts = driver.find_element_by_xpath('//*[@id="c2VuZE9nb2xsb3dlcnNfdGlrdG9r"]/span').text
-        sleep(55)
-        print(hearts," Success delivered!")
-        sleep(100)
-        driver.refresh()
-        sleep(200)
-        loop2()
-    except:
-        print("An error occured. Now will retry again")
-        driver.refresh()
-        sleep(355)
-        loop2()
+            self.user_id = DATA["id"]
+            self.username = DATA["uniqueId"]
+            self.secuid = DATA["secUid"]
+            
+        asyncio.get_event_loop().run_until_complete(self.start())
 
-def loop3():
-    pass
+    async def start(self):
+        threading.Thread(target=self.title).start()
+        while True:
+            async with tasksio.TaskPool(workers=1_000) as pool: 
+                for x in range(1_000): await pool.put(self.report())
+        
+    async def report(self):
+        while True:
+            try:
+                async with httpx.AsyncClient() as client:
+                    client.headers["user-agent"] = "Mozilla/5.0 (Linux; U; Android 4.4.2; en-us; SCH-I535 Build/KOT49H) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30"
+                    req = await client.get(f'https://www.tiktok.com/aweme/v1/aweme/feedback/?aid=1988&app_language=TK&channel={random.choice(["tiktok_web", "googleplay", "App%20Store"])}&current_region=TK&device_id={random.randint(1000000000000000000, 9999999999999999999)}&lang=en&nickname={self.username}&object_id=76493735542&os=1337&owner_id={self.user_id}&reason={self.report_reason}&region=TK&report_type=user&secUid={self.secuid}&target={self.user_id}&tz_name=vast1337')
+                    self.requests += 1
+                    if "Thanks for your feedback" in req.text:
+                        self.report_count += 1
+                        self.print(pystyle.Colorate.Horizontal(pystyle.Colors.blue_to_red, f' [ + ] Sent Report. ({self.report_count})', 1))
+                    else:  await self.report()
+            except: await self.report()
 
-def loop4():
-    sleep(20)
-    wait_time = 660 #11 minutes
-    try:
-        driver.find_element_by_xpath("/html/body/div[4]/div[1]/div[3]/div/div[1]/div/button").click() #Followers
-    except:
-        print("You didn't solve the captcha yet. Need to refresh to avoid endless loop.")
-        driver.refresh()
-        loop4()
-    try:
-        sleep(20)
-        driver.find_element_by_xpath("/html/body/div[4]/div[2]/div/div/div/form/div/input").send_keys(vidUrl) #Write
-        sleep(2)
-        driver.find_element_by_xpath("//*[@id=\"sid\"]/div/div/div/form/div/div/button").click() #Search
-        sleep(20)
-        driver.find_element_by_xpath("//*[@id=\"c2VuZF9mb2xsb3dlcnNfdGlrdG9r\"]/div[1]/div/form/button").click() #AddFollowers
-        sleep(wait_time/3)
-        print("Success delivered")
-        sleep(wait_time/3)
-        driver.refresh()
-        sleep(wait_time/3)
-        loop4()
-    except:
-        print("Oops!", sys.exc_info()[0], "occurred.")
-        print("An error occurred. Now will retry again")
-        driver.refresh()
-        sleep(wait_time)
-        loop4()
 
-vidUrl = "https://www.tiktok.com/@social_degradation_crazy/video/6890937108874169601" #Change with one of your tiktok videos
-
-system("clear") #If you have Windows you can use cls
-tiktokbot = pyfiglet.figlet_format("NoNameoN", font="slant")
-print(tiktokbot)
-print("Author: https://github.com/NoNameoN-A")
-print("")
-
-"""
-You can change auto value below
-auto = 1 for auto views OK
-auto = 2 for auto hearts OK
-auto = 3 for auto views + hearts InWork...
-auto = 4 for auto followers OK
-"""
-bot = 1 #Change this
-
-driver.get("https://vipto.de/")
-
-if bot == 1:
-    loop1()
-elif bot == 2:
-    loop2()
-elif bot == 3:
-    pass
-else:
-    loop4()
-    pass
-
+if __name__ == '__main__':
+    TikTok()
